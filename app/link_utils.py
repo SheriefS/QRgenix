05179de9b9e7264d60ea2d6c253b9dc002ab15d2
SheriefS <sheriefs@gmail.com>
2025-05-03 15:47:48 -0400

    Rebranding, Unit Testing added

--- app/link_utils.py
+++
@@ -1,6 +0,0 @@
-import re
-
-def clean_google_doc_url(url):
-    match = re.search(r"(https://docs\.google\.com/document/d/[^/]+)", url)
-    match = re.search(r"(https://drive\.google\.com/file/d/[^/]+)", url)
-    return match.group(1) + "/" if match else url
---
+++ tests/test_link_utils.py
@@ -0,0 +1,23 @@
+# tests/test_link_utils.py
+
+from app.utils.link_utils import clean_url
+
+def test_clean_google_doc_url():
+    input_url = "https://docs.google.com/document/d/abc123/edit?usp=sharing"
+    expected = "docs.google.com/document/d/abc123/"
+    assert clean_url(input_url) == expected
+
+def test_clean_google_drive_url():
+    input_url = "https://drive.google.com/file/d/xyz789/view?usp=drive_link"
+    expected = "drive.google.com/file/d/xyz789/"
+    assert clean_url(input_url) == expected
+
+def test_clean_non_matching_url():
+    input_url = "https://example.com/page"
+    expected = "example.com/page"
+    assert clean_url(input_url) == expected
+
+def test_clean_http_url():
+    input_url = "http://example.com/page"
+    expected = "example.com/page"
+    assert clean_url(input_url) == expected

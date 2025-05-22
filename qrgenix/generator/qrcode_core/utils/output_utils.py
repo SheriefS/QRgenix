1b7ece9b9a3891bae01a018924176ae1e6ae71f8
SheriefS <sheriefs@gmail.com>
2025-05-21 23:17:54 -0400

    Refactor of code to add Django framework

---
+++ qrgenix/generator/qrcode_core/utils/output_utils.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+
+def base_dir():
+    return Path(__file__).resolve().parents[1]
+
+def save_img(qr_img, filename):   
+    output_dir = os.path.join(base_dir(), 'output')
+    #Create folder if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, filename)
+    qr_img.save(output_path)
+    return os.path.abspath(output_path)

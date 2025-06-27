11f2c5eae5a56c4ce0c204d7b6baabd63d6d4e50
SheriefS <sheriefs@gmail.com>
2025-06-27 11:33:55 -0400

    Backend debug, added health status page to Django

--- backend-django/generator/urls.py
+++ backend-django/generator/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import generate_qr_api
-
+from .views import health_check
 
 urlpatterns = [
-    path("generate/", generate_qr_api, name="generate_qr_api"),  
+    path("generate/", generate_qr_api, name="generate_qr_api"),
+    path("health/", health_check, name="health-check"),  
 ]
